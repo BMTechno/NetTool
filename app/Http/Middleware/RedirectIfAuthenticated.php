2e1842569b7dce5c0d549964f848b9fb909b5773
Frozen <marto_dimitrov@mail.bg>
2016-07-08 16:35:49 +0300

    redirect fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/equimpents');
+            return redirect('/equipments');
         }
 
         return $next($request);
